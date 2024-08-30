17bf5238ebb6552371ba2f9ab85e068197d7a65d
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-30 14:37:16 +0430

    type aliases

--- myObject/object.ts
+++ myObject/object.ts
@@ -5,3 +5,18 @@ const createUser=({name:string,isPaid:boolean}):{}=>{
 const createCourse=():{name:string,price:number}=>{
     return {name:'barbod',price:20}
 }  //only return an object with two key name and price
+
+
+
+//type Aliases
+type User={
+    name:string;
+    email:string;
+    isPaid:boolean
+}
+
+const getInfo=(user:User):User=>{
+    return {name:'',email:'',isPaid:true}}
+
+
+getInfo({name:'',email:'',isPaid:true})
