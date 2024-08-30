80735c843ec0c0c83788a6c48a11541cce7011b9
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-30 17:36:50 +0430

    enums

---
+++ enums/enums.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice[SeatChoice["FIRST"] = 0] = "FIRST";
+    SeatChoice[SeatChoice["WINDOW"] = 1] = "WINDOW";
+    SeatChoice[SeatChoice["MIDDLE"] = 2] = "MIDDLE";
+})(SeatChoice || (SeatChoice = {}));
+var locSeat = SeatChoice.WINDOW;
---
+++ enums/enums.ts
@@ -0,0 +1,9 @@
+enum SeatChoice {
+    FIRST,//default start the number of first variable is zero (0) if you want change the start number just do this FIRST = 10 
+    WINDOW,
+    MIDDLE,
+}
+
+const locSeat = SeatChoice.WINDOW
+
+export {}
