956ac156ae099bbc37b4ac6bf654ebf13aea2fb6
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-30 12:18:40 +0430

    function

---
+++ function/function.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var func = function (number) {
+    return number + 2;
+};
+func(5);
+var toUpper = function (value) {
+    return value.toUpperCase();
+};
+toUpper("barbod");
+var user = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+user('barbod', 'barbod@gmail.com');
+//////////////////////////////////////// part two
+var addNumber = function (number) {
+    // return number+2
+    return "hello";
+};
+var value = addNumber(5);
---
+++ function/function.ts
@@ -0,0 +1,44 @@
+const func=(number:number)=>{
+    return number+2
+}
+func(5)
+
+const toUpper=(value:string)=>{
+    return value.toUpperCase()
+}
+toUpper("barbod")
+
+const user=(name:string,email:string,isPaid:boolean=false)=>{}
+user('barbod','barbod@gmail.com')
+//////////////////////////////////////// part two
+const addNumber=(number:number):number=>{
+    return number+2
+    // return "hello"
+}
+const value =addNumber(5)
+
+// const heros=['thor','spiderMan','ironMan']
+const heros=[1,2,5,5]
+
+heros.map((e:number):string=>{
+    return `name is : ${e}`
+})
+
+const consoleError=(error:string):void=>{
+    console.log(error)
+}// void mean this function doesn't return
+
+const handleError=(errorMsg:string):never=>{
+    throw new Error(errorMsg)
+}// never mean this function never return any thing
+
+
+
+
+
+
+
+
+
+
+export {}
