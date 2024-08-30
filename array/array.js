850483dd9f54781d25952c3bf29bde170df9d100
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-30 15:35:44 +0430

    array

---
+++ array/array.js
@@ -0,0 +1,4 @@
+var heros = [];
+heros.push({ name: 'barbod' });
+var barbodHero = [];
+barbodHero.push('barbod');
---
+++ array/array.ts
@@ -0,0 +1,25 @@
+const heros:object[]=[]
+heros.push({name:'barbod'})
+
+const barbodHero:string[]=[]
+barbodHero.push('barbod')
+
+const newArray:Array<number>=[]
+newArray.push(2)
+
+type User={
+    username:string
+    password:number
+}
+
+const users:Array<User>=[]
+users.push({username:'',password:3})
+
+
+
+
+
+
+
+
+export {}
