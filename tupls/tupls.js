01c5dece2a7a43894f2ffabeaa16f86776a39fa1
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-30 17:13:03 +0430

    tutupls

---
+++ tupls/tupls.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user;
+// user=[123,'barbod',true]//wrong
+user = ['barbod', 123]; //correct
+user.push(123);
---
+++ tupls/tupls.ts
@@ -0,0 +1,18 @@
+let user:[string,number]
+
+// user=[123,'barbod',true]//wrong
+user=['barbod',123]//correct
+user.push(123)
+
+// it's work with this methods : push pop unshift shift splice 
+
+
+
+
+
+
+
+
+
+
+export {}
