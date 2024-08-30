01045e6f7ead8ba80e1389e4e627f1a92ee7774f
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-30 12:45:19 +0430

    object with function

---
+++ myObject/object.ts
@@ -0,0 +1,7 @@
+const createUser=({name:string,isPaid:boolean}):{}=>{
+    return {}
+}
+
+const createCourse=():{name:string,price:number}=>{
+    return {name:'barbod',price:20}
+}  //only return an object with two key name and price
